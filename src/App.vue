a6b61be45637dd509645120ace1bb8f7319c46d0
Linus Grebing <linus.frederik.grebing@iem.thm.de>
2022-07-06 15:18:03 +0200

    firstCommit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class=" bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black py-4">
+       <h1 class="text-3xl py-4">ToDo-Liste</h1>
+       <p class="text-xl" v-if="openTodos.length > 0">Offene ToDo's: {{ openTodos.length }}</p>
+        <p class="text-xl" v-else>Momentan keine ToDos zu erledigen!</p>
+       <div class="m-4">
+        <input type="text" class="py-2 w-2/3 text-black" v-model="newTodo"/>
+        <button class="bg-red-400 py-2 w-1/3" @click="addToDo">Add</button> 
+       </div>
+    </div>
+    
+    <div >
+      <table>
+    <tr>
+      <th><b>Name &nbsp;</b></th>
+      <th><b>Rating &nbsp;</b> </th>
+      <th><b>Delete </b> </th>
+    </tr>
+    <tr v-for="(todo, index) in todos" :key="todo.name">
+     <ToDoApp 
+       :todosprob = "todos"
+       :todoprop="todo" 
+       :todoindex="index" 
+       @toggledone-index="toggleDone(index)"
+       @removetodo-index="removeToDo(index)"
+       />
+    </tr>
+  
+  </table>
+      
+
+    </div>
+   <button @click="alertName()" >Sort by first name</button>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoApp from './components/ToDoApp.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    }
+  },  
+  methods: {
+    alertName: function(){
+        this.todos.name.sort(); /** sort function is throwing an error here **/
+      console.log(this.todos.name); /** and this does not format array objects followed by first name **/
+    },
+  toggleDone(index){
+      this.todos[index].done = !this.todos[index].done;
+      this.storeTodos();
+    },
+    removeToDo(index){
+      this.todos.splice(index, 1); 
+      this.storeTodos();
+    },
+    addToDo(){
+      if(this.newTodo.trim() == ""){
+        return;
+      }
+      this.todos.push({name: this.newTodo, done: false });
+      this.storeTodos();
+    },
+    storeTodos(){
+      localStorage.setItem("todos",JSON.stringify(this.todos))
+    }
+  },
+  mounted(){
+    let data = localStorage.getItem("todos");
+    if(data !== "" && data !== null){
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    ToDoApp,
+    
+  },
+  computed: {
+    openTodos(){
+
+      const openToDos = this.todos.filter((name) => {
+        return !name.done
+      });
+      return openToDos;
+    },
+   
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ToDoApp.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="flex justify-between items-center py-2 px-2" :class="{'bg-green-600': this.myToDo.done, 'bg-red-600': !this.myToDo.done}">
+
+     <table>
+    <tr>
+      <th><b>Name &nbsp;</b></th>
+      <th><b>Rating &nbsp;</b> </th>
+      <th><b>Delete </b> </th>
+    </tr>
+    <tr v-for="myToDo in myToDoArray" :key="myToDo.name">
+      <td>{{ myToDo.name }}</td>
+      <td>{{ count }}</td>
+      <td> <button class="bg-black py-1 px-1 rounded" @click="removeToDo">Delete ToDo</button></td>
+    </tr>
+  
+  </table>
+    <!--<h1 class="cursor-pointer" @click="toggleTodo" >{{ myToDo.name }}</h1>
+    
+    <div class="flex items-center ml-auto px-6 ">
+        <h1 class="mx-3">{{ count }} / 5</h1>
+        <button class="bg-black p-2 rounded" v-if="count < 5" @click="countUp"><div class="h-0 w-0 border-x-8 border-x-transparent border-b-[16px] border-b-white-600"></div></button>
+        <button class="bg-black p-2 rounded" v-else><div class="h-0 w-0 border-x-8 border-b-[16px] border-b-white-600 border-opacity-5"></div></button>
+        <button class="bg-black p-2 rounded mx-1" v-if="count > 0"  @click="countDown"><div class="h-0 w-0 border-x-8 border-x-transparent border-t-[16px] border-b-white-600"></div></button>
+        <button class="bg-black p-2 rounded mx-1" v-else><div class="h-0 w-0 border-x-8  border-t-[16px]"></div> </button>
+     </div>
+    
+  -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoApp',
+  props: ["todoprop", "todoindex", "todosprob"],
+ /* data: function(){
+    return{
+      message: "Hello World",
+      count: 0,
+    };
+  },
+  */
+  data(){
+    return {
+      count: 0,
+      myToDo: this.todoprop,
+      myToDoArray: this.todosprob
+
+    };
+  },
+  methods: {
+    
+    countUp(){
+        this.count++; 
+      },
+
+    countDown(){
+      this.count--;
+    },
+  
+    toggleTodo(){
+      this.$emit("toggledone-index");
+    },
+    removeToDo() {
+      this.$emit("removetodo-index");
+    }
+  },
+  computed: {
+   
+  }
+};
+</script>
+
+
